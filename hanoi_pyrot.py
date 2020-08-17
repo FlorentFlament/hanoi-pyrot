44c65b7d324d2d8984625e6314df1e6d70702fca
Florent Flament <contact@florentflament.com>
2020-08-17 07:24:12 +0200

    Save state between executions

--- hanoi_pyrot.py
+++ hanoi_pyrot.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
+import json # to store and load state
+
+DEFAULT_STATE_FILENAME="hanoi_pyrot.state"
 
 class HanoiRotation:
-    def __init__(self, n_pools, tapes_per_pool):
-        self.__n_pools = n_pools
-        self.__tapes_per_pool = tapes_per_pool
+    def from_scratch(n_pools, tapes_per_pool):
+        hanoi = HanoiRotation()
+        hanoi.__n_pools = n_pools
+        hanoi.__tapes_per_pool = tapes_per_pool
         # Stack will contain (verb, pool) couples
         # * verb is 'e' (execute) or 'v' (value) - yes not a real verb
         # * pool is a pool id
-        self.__stack = []
-        self.__tapes = [0] * self.__n_pools
+        hanoi.__stack = []
+        hanoi.__tapes = [0] * n_pools
+        return hanoi
+
+    def from_statefile(fname=DEFAULT_STATE_FILENAME):
+        hanoi = HanoiRotation()
+        hanoi.__load_state(fname)
+        return hanoi
 
     def __next_pool(self):
         while True:
@@ -17,53 +27,62 @@ class HanoiRotation:
                 return self.__n_pools-1
 
             verb, pool = self.__stack.pop()
-            
+
             if verb == 'e':
                 if pool == 0:
                     self.__stack.append(['v', 0])
-                    
+
                 else:
                     self.__stack.append(['e', pool-1])
                     self.__stack.append(['v', pool])
                     self.__stack.append(['e', pool-1])
-                
+
             elif verb == 'v':
                 return pool
 
             else:
                 raise ValueError("Unknown verb: '{}'".format(verb))
-            
-    def __next_pool_tape(self, pool_id):
+
+    def __next_tape(self, pool_id):
         tape = self.__tapes[pool_id]
         self.__tapes[pool_id] = (self.__tapes[pool_id] + 1) % self.__tapes_per_pool
         return tape
-        
-    def next_tape(self):
+
+    def next(self):
         """
         Returns a couple (pool_id, pool_tape_id)
         """
         pool = self.__next_pool()
-        tape = self.__next_pool_tape(pool)
+        tape = self.__next_tape(pool)
         return (pool, tape)
+
+    def __load_state(self, fname=DEFAULT_STATE_FILENAME):
+        with open(fname, 'r') as fd:
+            for k,v in json.load(fd).items():
+                setattr(self, k, v)
+
+    def save_state(self, fname=DEFAULT_STATE_FILENAME):
+        with open(fname, 'w') as fd:
+            json.dump(self.__dict__, fd, sort_keys=True)
 
 
 def test_4pools_1tape():
     expected = [(3,0), (0,0), (1,0), (0,0), (2,0), (0,0), (1,0), (0,0),
                 (3,0), (0,0), (1,0), (0,0), (2,0), (0,0), (1,0), (0,0),
                 (3,0), (0,0), (1,0), (0,0), (2,0), (0,0), (1,0), (0,0)]
-    hanoi = HanoiRotation(4, 1)
+    hanoi = HanoiRotation.from_scratch(4, 1)
     for i, v in enumerate(expected):
-        t = hanoi.next_tape()
+        t = hanoi.next()
         if v != t:
             print("*Error* at iteration {}: expected tape '{}' but got '{}'".format(i, v, t))
-    
+
 def test_4pools_2tapes():
     expected = [(3,0), (0,0), (1,0), (0,1), (2,0), (0,0), (1,1), (0,1),
                 (3,1), (0,0), (1,0), (0,1), (2,1), (0,0), (1,1), (0,1),
                 (3,0), (0,0), (1,0), (0,1), (2,0), (0,0), (1,1), (0,1)]
-    hanoi = HanoiRotation(4, 2)
+    hanoi = HanoiRotation.from_scratch(4, 2)
     for i, v in enumerate(expected):
-        t = hanoi.next_tape()
+        t = hanoi.next()
         if v != t:
             print("*Error* at iteration {}: expected tape '{}' but got '{}'".format(i, v, t))
 
@@ -75,9 +94,12 @@ def test():
     print("done")
 
 def main():
-    hanoi = HanoiRotation(20, 2)
-    for i in range(365):
-        print(i, hanoi.next_tape())
-
+    try:
+        hanoi = HanoiRotation.from_statefile()
+    except FileNotFoundError:
+        hanoi = HanoiRotation.from_scratch(20, 2)
+    pool, tape = hanoi.next()
+    print("{}-{}".format(pool, tape))
+    hanoi.save_state()
 
 main()
